379b52c329112490a09b048d89fc7622b914775e
CarlosVargas16 <carvargasr16@gmail.com>
2025-11-21 17:44:28 -0600

    Create ejercicio9.c

---
+++ ejercicio9.c
@@ -0,0 +1,15 @@
+/*Vargas Rodriguez Carlos Antonnio
+Programa 9*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Ingrese un numero: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n * i);
+
+    return 0;
+}
