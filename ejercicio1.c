492096515678b6f7acf8652bdf2bdeaf9722a280
CarlosVargas16 <carvargasr16@gmail.com>
2025-11-21 13:32:22 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,16 @@
+/*Vargas Rodriguez Carlos Antonnio
+Programa 1*/
+
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    printf("Ingrese dos numeros: ");
+    scanf("%f %f", &a, &b);
+
+    printf("Suma: %.2f\n", a + b);
+    printf("Resta: %.2f\n", a - b);
+    printf("Multiplicacion: %.2f\n", a * b);
+
+    return 0;
+}
